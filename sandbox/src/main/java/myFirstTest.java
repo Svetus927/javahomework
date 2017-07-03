7b281d422e77540c08f6ccf2479fd83df72e9226
Svetus927 <nikitisvetlana@gmail.com>
2017-07-03 18:42:21 +0200

    Task1 + Task2

--- sandbox/src/main/java/myFirstTest.java
+++ sandbox/src/main/java/myFirstTest.java
@@ -1,6 +1,7 @@
 
 public class myFirstTest {
-    public static void main(String[] args) {
-        System.out.println("Hello");
-    }
+  /*  public static void main(String[] args) {
+        System.out.println("Hello my first test" );
+        System.out.println();
+    } */
 }
